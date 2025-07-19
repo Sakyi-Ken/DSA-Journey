e8d67737288ea7f3b981da601ee3f4ec15b27096
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-07-19 13:31:22 +0000

    Implement Editor and History classes with text manipulation and action management features

---
+++ code-signal/Lesson-4/editor.py
@@ -0,0 +1,72 @@
+class Editor:
+  def __init__(self):
+    self.text = ""
+    self.history_stack = []
+    self.redo_stack = []
+
+  def append_text(self, text):
+    self.history_stack.append(self.text)
+    self.text += text
+
+  def undo(self):
+    if self.history_stack:
+      self.redo_stack.append(self.text)
+      self.text = self.history_stack.pop()
+    else:
+      print("Undo operation not possible. No history available.")
+
+  def redo(self):
+    if self.redo_stack:
+      self.history_stack.append(self.text)
+      self.text = self.redo_stack.pop()
+    else:
+      print("Redo operation not possible. No redo history available.")
+
+  def display_text(self):
+    print(self.text)
+
+
+editor = Editor()
+
+editor.append_text("Hello, ")
+editor.append_text("CodeSignal!")
+editor.display_text()
+editor.undo()
+editor.display_text()
+editor.undo()
+editor.display_text()
+editor.redo()
+editor.display_text()
+editor.redo()
+editor.display_text()
+editor.redo()
+editor.undo()
+
+
+class History:
+  def __init__(self):
+    self.history_stack = []
+    self.future_stack = []
+
+  def execute_action(self, action):
+    self.history_stack.append(action)
+    print(f"Executing: {action}")
+
+  def undo_action(self):
+    if self.history_stack:
+      self.future_stack.append(self.history_stack.pop())
+      action = self.history_stack[-1]
+      print(f"Undid, now on action: {action}")
+    else:
+      print("Nothing to undo. Initialize a new action first.")
+
+  def redo_action(self):
+    if self.future_stack:
+      action = self.future_stack.pop()
+      self.history_stack.append(action)
+      print(f"Redid, now on action: {action}")
+    else:
+      print("Nothing to redo")
+
+history = History()
+history.undo_action()
